6b9907917e5256035a7104d6773b4bbd38140644
James Halliday <mail@substack.net>
2013-01-10 10:47:58 -0800

    implicit tests

--- test/binary.js
+++ test/binary.js
@@ -15,3 +15,10 @@ test('binary', function (t) {
     t.same(gray(2, 2), codes[2], '2-bit binary');
     t.same(gray(3, 2), codes[3], '3-bit binary');
 });
+
+test('implicit binary', function (t) {
+    t.same(gray(0), codes[0], '0-bit binary');
+    t.same(gray(1), codes[1], '1-bit binary');
+    t.same(gray(2), codes[2], '2-bit binary');
+    t.same(gray(3), codes[3], '3-bit binary');
+});
