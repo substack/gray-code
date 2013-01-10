5fa52eb214db50913aaebf3b503d096620ed3dc8
James Halliday <mail@substack.net>
2013-01-10 11:26:00 -0800

    remove custom terms

--- example/terms.js
+++
@@ -1,3 +0,0 @@
-var gray = require('../');
-var codes = gray(2, ['snake','turtle','wolf','eagle']);
-console.dir(codes);
--- index.js
+++ index.js
@@ -1,32 +1,23 @@
-module.exports = function gray (bits, terms) {
-    if (bits === 0) return [];
-    if (terms === undefined) terms = [ 0, 1 ];
-    if (typeof terms === 'number') {
-        var terms_ = [];
-        for (var i = 0; i < terms; i++) terms_.push(i);
-        terms = terms_;
-    }
-    if (bits === 1) return terms.map(lift);
+module.exports = function gray (n, k) {
+    if (k === undefined) k = 2;
+    if (n === 0) return [];
     
-    var radix = terms.length;
     var codes = [];
-    var max = Math.pow(radix, bits);
+    var max = Math.pow(k, n);
     
-    for (var n = 0; n < max; n++) {
+    for (var i = 0; i < max; i++) {
         var res = [];
         var shift = 0;
         
-        for (var j = bits - 1; j >= 0; j--) {
-            var x = (digit(n, radix, j) + shift) % radix;
-            shift += radix - x;
-            res.push(terms[x]);
+        for (var j = n - 1; j >= 0; j--) {
+            var x = (digit(i, k, j) + shift) % k;
+            shift += k - x;
+            res.push(x);
         }
         codes.push(res);
     }
     return codes;
 };
-
-function lift (x) { return [x] }
 
 function digit (n, radix, i) {
     return Math.floor(n / Math.pow(radix, i)) % radix;
