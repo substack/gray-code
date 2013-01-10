4c12fe877d2071cf6b386d7d0adc77052bdfac50
James Halliday <mail@substack.net>
2013-01-10 11:36:42 -0800

    fix test

--- test/binary.js
+++ test/binary.js
@@ -17,6 +17,7 @@ test('binary', function (t) {
 });
 
 test('implicit binary', function (t) {
+    t.plan(4);
     t.same(gray(0), codes[0], '0-bit binary');
     t.same(gray(1), codes[1], '1-bit binary');
     t.same(gray(2), codes[2], '2-bit binary');
